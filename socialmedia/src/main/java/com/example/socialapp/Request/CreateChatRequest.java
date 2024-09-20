5ca75ee77b3e31e4b8e6c941730f3369af72d28b
locnpIT <npl2004qn@gmail.com>
2024-09-21 02:22:41 +0700

    upload code

---
+++ socialmedia/src/main/java/com/example/socialapp/Request/CreateChatRequest.java
@@ -0,0 +1,25 @@
+package com.example.socialapp.Request;
+
+import com.example.socialapp.Models.User;
+
+public class CreateChatRequest {
+
+	
+	private Integer userId;
+	
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	
+	
+	
+	
+	
+	
+
+}
